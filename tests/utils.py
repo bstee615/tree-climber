8a4ef7aba2a83e040963b50ce93c00c3983e5775
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-06-15 13:53:31 -0500

    Draw double edges as arcs and single edges as straight lines

--- tests/utils.py
+++ tests/utils.py
@@ -11,7 +11,11 @@ def parse_and_create_cfg(code):
 
 def draw(cfg, dataflow_solution=None):
     pos = nx.spring_layout(cfg, seed=0)
-    nx.draw(cfg, pos=pos, connectionstyle=f"arc3,rad=0.1")
+    nx.draw_networkx_nodes(cfg, pos=pos)
+    double_edges = [(u, v) for u, v in cfg.edges() if u in cfg.successors(v) and v in cfg.successors(u)]
+    single_edges = [e for e in cfg.edges() if e not in double_edges]
+    nx.draw_networkx_edges(cfg, pos=pos, edgelist=single_edges)
+    nx.draw_networkx_edges(cfg, pos=pos, edgelist=double_edges, connectionstyle=f"arc3,rad=0.1")
     nx.draw_networkx_labels(cfg, pos=pos, labels={n: attr.get("label", "<NO LABEL>") for n, attr in cfg.nodes(data=True)})
     if dataflow_solution is not None:
         nx.draw_networkx_labels(cfg, pos=pos, font_color="r", labels={n: "\n\n\n{" + ", ".join(dataflow_solution[n]) + "}" if dataflow_solution[n] else "" for n in cfg.nodes()})
