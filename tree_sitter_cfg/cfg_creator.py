56e6bbe3eed08a9c365ad693161a6278a75c283f
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-06-15 11:18:29 -0500

    Add if statement tests

--- tree_sitter_cfg/cfg_creator.py
+++ tree_sitter_cfg/cfg_creator.py
@@ -147,6 +147,35 @@ class CFGCreator(BaseVisitor):
         self.add_edge_from_fringe_to(cond_id)
         self.fringe.append(cond_id)
 
+def test_if_noelse():
+    tree = c_parser.parse(bytes("""int main()
+    {
+        if (x > 1) {
+            x += 5;
+        }
+    }
+    """, "utf8"))
+    v = CFGCreator()
+    v.visit(tree.root_node)
+    assert (v.cfg.number_of_nodes(), v.cfg.number_of_edges()) == (4, 4)
+    assert nx.is_directed_acyclic_graph(v.cfg)
+
+def test_if_else():
+    tree = c_parser.parse(bytes("""int main()
+    {
+        if (x > 1) {
+            x += 5;
+        }
+        else {
+            x += 50;
+        }
+    }
+    """, "utf8"))
+    v = CFGCreator()
+    v.visit(tree.root_node)
+    assert (v.cfg.number_of_nodes(), v.cfg.number_of_edges()) == (5, 5)
+    assert nx.is_directed_acyclic_graph(v.cfg)
+
 def test_for_simple():
     tree = c_parser.parse(bytes("""int main()
     {
