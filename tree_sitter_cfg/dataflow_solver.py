435153d510520f63475dae99bccef259b4055b16
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-06-15 13:33:50 -0500

    Refactor to class

--- tree_sitter_cfg/dataflow_solver.py
+++ tree_sitter_cfg/dataflow_solver.py
@@ -22,83 +22,90 @@ def get_definition(ast_node):
             # rhs = assignment.children[2].text
     return identifier
 
-def solve(cfg, verbose=0):
+class ReachingDefinitionProblem:
     """
     reaching definition
     worklist algorithm
     https://en.wikipedia.org/wiki/Reaching_definition
     """
 
-    def gen(n, node2def) -> set:
-        if n in node2def:
-            if verbose >= 2:
-                print("gen", n, node2def[n])
-            return set([node2def[n]])
+    def __init__(self, cfg, verbose):
+        self.cfg = cfg
+        self.verbose = verbose
+
+        node2def = {}
+        id2def = {}
+        def2id = {}
+        def_idx = 0
+        for n in nx.dfs_preorder_nodes(cfg, source=cfg.graph["entry"]):
+            ast_node = cfg.nodes[n]["n"]
+            _id = get_definition(ast_node)
+            if _id is not None:
+                node2def[n] = def_idx
+
+                if _id not in id2def:
+                    id2def[_id] = set()
+                id2def[_id].add(def_idx)
+                def2id[def_idx] = _id
+
+                def_idx += 1
+        if verbose >= 1:
+            print("node2def", node2def)
+            print("def2id", def2id)
+        self.node2def = node2def
+        self.id2def = id2def
+        self.def2id = def2id
+
+    def gen(self, n) -> set:
+        if n in self.node2def:
+            d = self.node2def[n]
+            if self.verbose >= 2:
+                print("gen", n, d)
+            return {d}
         else:
             return set()
 
-    def kill(n, node2def, id2def, def2id) -> set:
-        if n in node2def:
-            d = node2def[n]
-            if d in def2id:
-                i = def2id[d]
-                if i in id2def:
-                    if verbose >= 2:
-                        print("kill", n, id2def[i])
-                    return id2def[i]
+    def kill(self, n) -> set:
+        if n in self.node2def:
+            d = self.node2def[n]
+            if d in self.def2id:
+                i = self.def2id[d]
+                if i in self.id2def:
+                    if self.verbose >= 2:
+                        print("kill", n, self.id2def[i])
+                    return self.id2def[i]
         else:
             return set()
 
-    node2def = {}
-    def2node = {}
-    id2def = {}
-    def2id = {}
-    def_idx = 0
-    for n in nx.dfs_preorder_nodes(cfg, source=cfg.graph["entry"]):
-        ast_node = cfg.nodes[n]["n"]
-        _id = get_definition(ast_node)
-        if _id is not None:
-            node2def[n] = def_idx
-            def2node[def_idx] = n
+    def solve(self):
+        _in = {}
+        out = {}
+        for n in self.cfg.nodes():
+            out[n] = set() # can optimize by OUT[n] = GEN[n];
 
-            if _id not in id2def:
-                id2def[_id] = set()
-            id2def[_id].add(def_idx)
-            def2id[def_idx] = _id
+        q = list(self.cfg.nodes())
+        i = 0
+        while q:
+            n = q.pop(0)
+            
+            _in[n] = set()
+            for pred in self.cfg.predecessors(n):
+                _in[n] |= out[pred]
+            
+            new_out_n = self.gen(n).union(_in[n].difference(self.kill(n)))
 
-            def_idx += 1
-    if verbose >= 1:
-        print("node2def", node2def)
-        print("def2id", def2id)
+            if self.verbose >= 2:
+                print(f"{i=}, {n=}, {_in=}, {out=}, {new_out_n=}")
 
-    _in = {}
-    out = {}
-    for n in cfg.nodes():
-        out[n] = set() # can optimize by OUT[n] = GEN[n];
-
-    q = list(cfg.nodes())
-    i = 0
-    while q:
-        n = q.pop(0)
+            if out[n] != new_out_n:
+                if self.verbose >= 1:
+                    print(f"{i=}, {n=} changed {out[n]} -> {new_out_n}")
+                out[n] = new_out_n
+                for succ in self.cfg.successors(n):
+                    q.append(succ)
+            i += 1
         
-        _in[n] = set()
-        for pred in cfg.predecessors(n):
-            _in[n] |= out[pred]
-        
-        new_out_n = gen(n, node2def).union(_in[n].difference(kill(n, node2def, id2def, def2id)))
-
-        if verbose >= 2:
-            print(f"{i=}, {n=}, {_in=}, {out=}, {new_out_n=}")
-
-        if out[n] != new_out_n:
-            if verbose >= 1:
-                print(f"{i=}, {n=} changed {out[n]} -> {new_out_n}")
-            out[n] = new_out_n
-            for succ in cfg.successors(n):
-                q.append(succ)
-        i += 1
-    
-    return out
+        return out
 
 def test_solve():
     code = """int main()
@@ -116,7 +123,7 @@ def test_solve():
     v = CFGCreator()
     v.visit(tree.root_node)
     cfg = v.cfg
-    solution = solve(cfg, verbose=1)
+    solution = ReachingDefinitionProblem(cfg, verbose=1).solve()
     print(solution)
     assert solution[next(n for n, attr in cfg.nodes(data=True) if "return" in attr["label"])] == {2}
     assert solution[next(n for n, attr in cfg.nodes(data=True) if "printf" in attr["label"])] == {0, 1}
