e9154e5b93f617765e24e12cc09aed0160b65fb2
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-06-15 13:37:06 -0500

    Refactor into base class

--- tree_sitter_cfg/dataflow_solver.py
+++ tree_sitter_cfg/dataflow_solver.py
@@ -8,7 +8,46 @@ An instance of a dataflow problem includes:
 """
 
 from tests.utils import *
-from tree_sitter_cfg.base_visitor import BaseVisitor
+
+class DataflowProblem:
+    """
+    generic dataflow problem solver with worklist algorithm
+    """
+    def __init__(self, cfg, verbose):
+        self.cfg = cfg
+        self.verbose = verbose
+
+    def solve(self):
+        _in = {}
+        out = {}
+        for n in self.cfg.nodes():
+            out[n] = set() # can optimize by OUT[n] = GEN[n];
+
+        q = list(self.cfg.nodes())
+        i = 0
+        while q:
+            n = q.pop(0)
+            
+            # TODO: generalize to backward problems
+            _in[n] = set()
+            for pred in self.cfg.predecessors(n):
+                _in[n] |= out[pred]
+            
+            # TODO: generalize to backward problems
+            new_out_n = self.gen(n).union(_in[n].difference(self.kill(n)))
+
+            if self.verbose >= 2:
+                print(f"{i=}, {n=}, {_in=}, {out=}, {new_out_n=}")
+
+            if out[n] != new_out_n:
+                if self.verbose >= 1:
+                    print(f"{i=}, {n=} changed {out[n]} -> {new_out_n}")
+                out[n] = new_out_n
+                for succ in self.cfg.successors(n):
+                    q.append(succ)
+            i += 1
+        
+        return out
 
 def get_definition(ast_node):
     identifier = None
@@ -22,16 +61,14 @@ def get_definition(ast_node):
             # rhs = assignment.children[2].text
     return identifier
 
-class ReachingDefinitionProblem:
+class ReachingDefinitionProblem(DataflowProblem):
     """
     reaching definition
-    worklist algorithm
     https://en.wikipedia.org/wiki/Reaching_definition
     """
 
     def __init__(self, cfg, verbose):
-        self.cfg = cfg
-        self.verbose = verbose
+        super().__init__(cfg, verbose)
 
         node2def = {}
         id2def = {}
@@ -76,36 +113,6 @@ class ReachingDefinitionProblem:
                     return self.id2def[i]
         else:
             return set()
-
-    def solve(self):
-        _in = {}
-        out = {}
-        for n in self.cfg.nodes():
-            out[n] = set() # can optimize by OUT[n] = GEN[n];
-
-        q = list(self.cfg.nodes())
-        i = 0
-        while q:
-            n = q.pop(0)
-            
-            _in[n] = set()
-            for pred in self.cfg.predecessors(n):
-                _in[n] |= out[pred]
-            
-            new_out_n = self.gen(n).union(_in[n].difference(self.kill(n)))
-
-            if self.verbose >= 2:
-                print(f"{i=}, {n=}, {_in=}, {out=}, {new_out_n=}")
-
-            if out[n] != new_out_n:
-                if self.verbose >= 1:
-                    print(f"{i=}, {n=} changed {out[n]} -> {new_out_n}")
-                out[n] = new_out_n
-                for succ in self.cfg.successors(n):
-                    q.append(succ)
-            i += 1
-        
-        return out
 
 def test_solve():
     code = """int main()
