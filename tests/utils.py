23c60b78aeaaee5f057b9c956e257a14261c32ce
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-06-23 07:16:09 -0500

    Finish implementation of def use graph

--- tests/utils.py
+++ tests/utils.py
@@ -4,16 +4,16 @@ from tree_sitter_cfg.tree_sitter_utils import c_parser
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def draw(cfg, dataflow_solution=None):
+def draw(cfg, dataflow_solution=None, ax=None):
     pos = nx.spring_layout(cfg, seed=0)
-    nx.draw_networkx_nodes(cfg, pos=pos)
+    nx.draw_networkx_nodes(cfg, pos=pos, ax=ax)
     double_edges = [(u, v) for u, v in cfg.edges() if u in cfg.successors(v) and v in cfg.successors(u)]
     single_edges = [e for e in cfg.edges() if e not in double_edges]
-    nx.draw_networkx_edges(cfg, pos=pos, edgelist=single_edges)
+    nx.draw_networkx_edges(cfg, pos=pos, edgelist=single_edges, ax=ax)
     nx.draw_networkx_edges(cfg, pos=pos, edgelist=double_edges, connectionstyle=f"arc3,rad=0.1")
-    nx.draw_networkx_labels(cfg, pos=pos, labels={n: attr.get("label", "<NO LABEL>") for n, attr in cfg.nodes(data=True)})
+    nx.draw_networkx_labels(cfg, pos=pos, labels={n: attr.get("label", "<NO LABEL>") for n, attr in cfg.nodes(data=True)}, ax=ax)
     if dataflow_solution is not None:
-        nx.draw_networkx_labels(cfg, pos=pos, font_color="r", labels={n: "\n\n\n{" + ", ".join(dataflow_solution[n]) + "}" if dataflow_solution[n] else "" for n in cfg.nodes()})
+        nx.draw_networkx_labels(cfg, pos=pos, font_color="r", labels={n: "\n\n\n{" + ", ".join(dataflow_solution[n]) + "}" if dataflow_solution[n] else "" for n in cfg.nodes()}, ax=ax)
     plt.show()
 
 
--- tree_sitter_cfg/dataflow/def_use.py
+++ tree_sitter_cfg/dataflow/def_use.py
@@ -6,35 +6,27 @@ import matplotlib.pyplot as plt
 from tests.utils import draw
 
 def get_def_use(cfg):
-    G = nx.DiGraph()
+    dug = nx.DiGraph()
+    dug.add_nodes_from(cfg.nodes(data=True))
     solver = ReachingDefinitionSolver(cfg, verbose=1)
-    solution = solver.solve()
-    print(solution)
-    # assert solution[next(n for n, attr in cfg.nodes(data=True) if "return" in attr["label"])] == {2}
-    # assert solution[next(n for n, attr in cfg.nodes(data=True) if "printf" in attr["label"])] == {0, 1}
-    # draw(cfg, {n: sorted(set(map(solver.def2code.__getitem__, b))) for n, b in solution.items()})
-    labeldict = {}
-    for n, attr in cfg.nodes(data=True):
-        if n in solver.node2def:
+    solution_in, solution_out = solver.solve()
+    solution = solution_in
+    for n in cfg.nodes():
+        incoming_defs = solution[n]
+        if any(incoming_defs):
             use_node = n
-            def_idx = solver.node2def[use_node]
-            def_node = solver.def2id[def_idx]
-            G.add_edge(use_node, def_node)
-            labeldict[n] = attr["label"]
-    # labeldict = nx.get_node_attributes(G, "label")
-    print(labeldict)
-    nx.draw(G, labels=labeldict, with_labels = True)
-    plt.show()
-    plt.close()
-    draw(cfg)
-    plt.show()
+            def_nodes = map(solver.def2node.__getitem__, incoming_defs)
+            for def_node in def_nodes:
+                # TODO: detect if this variable is used in this node
+                dug.add_edge(def_node, use_node)
+    return dug
 
 
 def test():
     code = """int main()
     {
         int x = 0;
-        if (true) {
+        for (; true; ) {
             x += 5;
         }
         printf("%d\\n", x);
@@ -45,5 +37,19 @@ def test():
     tree = c_parser.parse(bytes(code, "utf8"))
     v = CFGCreator()
     cfg = v.generate_cfg(tree.root_node)
-    get_def_use(cfg)
+    dug = get_def_use(cfg)
+
+    assignment_0_node = next(n for n, attr in dug.nodes(data=True) if "x = 0" in attr["label"])
+    plus_assignment_5_node = next(n for n, attr in dug.nodes(data=True) if "x += 5" in attr["label"])
+    assignment_10_node = next(n for n, attr in dug.nodes(data=True) if "x = 10" in attr["label"])
+    printf_node = next(n for n, attr in dug.nodes(data=True) if "printf" in attr["label"])
+    return_node = next(n for n, attr in dug.nodes(data=True) if "return" in attr["label"])
+    assert not any(dug.predecessors(assignment_0_node))  # first assignment to x
+    assert set(dug.predecessors(printf_node)) == {plus_assignment_5_node, assignment_0_node}  # printf can print x defined by "x = 0" or "x += 5"
+    assert set(dug.predecessors(return_node)) == {assignment_10_node}  # return is dominated by "x = 10"
+    assert set(dug.predecessors(plus_assignment_5_node)) == {plus_assignment_5_node, assignment_0_node}  # TODO: += should take into account its predecessor x
+
+    _, ax = plt.subplots(2)
+    nx.draw(dug, labels={n: attr["label"] for n, attr in dug.nodes(data=True)}, with_labels = True, ax=ax[0])
+    draw(cfg, ax=ax[1])
 
