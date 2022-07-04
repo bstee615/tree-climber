d3e35ca0d1bf384410c386672509fe67044a00a6
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-07-04 10:59:19 -0500

    Label for statement components

--- tree_sitter_cfg/ast_creator.py
+++ tree_sitter_cfg/ast_creator.py
@@ -4,6 +4,11 @@ import networkx as nx
 from tree_sitter_cfg.base_visitor import BaseVisitor
 from tree_sitter_cfg.tree_sitter_utils import c_parser
 
+def assert_boolean_expression(n):
+    assert n.type.endswith("_statement") or n.type.endswith("_expression") or n.type in ("true", "false", "identifier"), n.type
+def assert_branch_target(n):
+    assert n.type.endswith("_statement") or n.type.endswith("_expression") or n.type in ("else",), n.type
+
 class ASTCreator(BaseVisitor):
     """
     AST visitor which creates a CFG.
@@ -15,36 +20,80 @@ class ASTCreator(BaseVisitor):
         self.ast = nx.DiGraph()
         self.node_id = 0
     
-    def make_ast(self, root_node):
-        self.visit(root_node, parent_id=None)
+    @staticmethod
+    def make_ast(root_node):
+        visitor = ASTCreator()
+        visitor.visit(root_node, parent_id=None)
+        return visitor.ast
 
-    def visit_default(self, n, parent_id):
+    def visit_for_statement(self, n, **kwargs):
+        children = n.children
+        i = 0
+        while children[i].type != "(":
+            i += 1
+        i += 1
+        if children[i].type == "declaration":
+            has_init = True
+            i += 1
+        elif children[i].type == ";":
+            has_init = False
+            i += 1
+        else:
+            assert children[i].type in ("assignment_expression", "comma_expression")
+            has_init = True
+            i += 1
+            assert children[i].type == ";"
+            i += 1
+        # pointing after semicolon
+        if children[i].type == ";":
+            has_cond = False
+        else:
+            assert_boolean_expression(children[i])
+            has_cond = True
+            i += 1
+        # pointing at semicolon
+        assert children[i].type == ";"
+        i += 1
+        if children[i].type == ")":
+            has_incr = False
+        else:
+            assert children[i].type in ("update_expression", "binary_expression", "call_expression")
+            has_incr = True
+        self.visit_default(n, has_init=has_init, has_cond=has_cond, has_incr=has_incr, **kwargs)
+
+    def visit_default(self, n, parent_id, **kwargs):
         code = n.text.decode()
         my_id = self.node_id
         self.node_id += 1
-        self.ast.add_node(my_id, n=n, label=code, code=code, node_type = n.type, start=n.start_point, end=n.end_point)
-        if parent_id is not None:
-            self.ast.add_edge(parent_id, my_id)
-        self.visit_children(n, parent_id=my_id)
+        if parent_id is None:
+            self.ast.graph["root_node"] = my_id
+        if n.is_named:
+            def attr_to_label(node_type, code):
+                lines = code.splitlines()
+                code = lines[0]
+                max_len = 27
+                trimmed_code = code[:max_len]
+                if len(lines) > 1 or len(code) > max_len:
+                    trimmed_code += "..."
+                return node_type + "\n" + trimmed_code
+            self.ast.add_node(my_id, n=n, label=attr_to_label(n.type, code), code=code, node_type=n.type, start=n.start_point, end=n.end_point, **kwargs)
+            if parent_id is not None:
+                self.ast.add_edge(parent_id, my_id)
+            self.visit_children(n, parent_id=my_id)
 
 def test():
-    code = """int main()
+    code = """#include <stdio.h>
+int main()
 {
+    int i = 0;
+    for (; i < 10; i ++) {
+        printf("%d\n", i);
+    }
     return 0;
 }
 """
     tree = c_parser.parse(bytes(code, "utf8"))
-    ast_v = ASTCreator()
-    ast_v.make_ast(tree.root_node)
-    ast = ast_v.ast
-    def attr_to_code(attr):
-        lines = attr["code"].splitlines()
-        code = lines[0]
-        max_len = 27
-        trimmed_code = code[:max_len]
-        if len(lines) > 1 or len(code) > max_len:
-            trimmed_code += "..."
-        return attr["node_type"] + ": " + trimmed_code
+    ast = ASTCreator.make_ast(tree.root_node)
     pos = nx.drawing.nx_agraph.graphviz_layout(ast, prog='dot')
-    nx.draw(ast, pos=pos, labels={n: attr_to_code(attr) for n, attr in ast.nodes(data=True)}, with_labels = True)
+    nx.draw(ast, pos=pos, labels={n: attr["label"] for n, attr in ast.nodes(data=True)}, with_labels = True)
     plt.show()
