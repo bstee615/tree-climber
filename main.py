ba6203eeca626a28ea82caf66a9954b43dab3f92
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-07-05 11:35:16 -0500

    Add DUC to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import traceback
 from tree_sitter_cfg.ast_creator import ASTCreator
 from tree_sitter_cfg.cfg_creator import CFGCreator
+from tree_sitter_cfg.dataflow.def_use import make_duc
 from tree_sitter_cfg.tree_sitter_utils import c_parser
 import networkx as nx
 from networkx.readwrite import json_graph
@@ -26,7 +27,7 @@ if __name__ == "__main__":
     parser.add_argument("filename", help="filename to parse")
     parser.add_argument("--draw_ast", action="store_true", help="draw AST (Abstract Syntax Tree)")
     parser.add_argument("--draw_cfg", action="store_true", help="draw CFG (Control-Flow Graph)")
-    parser.add_argument("--draw_cfg", action="store_true", help="draw CFG")
+    parser.add_argument("--draw_duc", action="store_true", help="draw DUC (Def-Use Chain)")
     parser.add_argument("--each_function", action="store_true", help="draw each function's CFG as a separate plot")
     parser.add_argument("--write_cfg", action="store_true", help="write CFG to file")
     args = parser.parse_args()
@@ -50,21 +51,28 @@ if __name__ == "__main__":
                 nx.draw(ast, pos=pos, labels={n: attr["label"] for n, attr in ast.nodes(data=True)}, with_labels = True)
                 plt.show()
 
-                cfg = CFGCreator.make_cfg(ast)
-                if args.draw_cfg:
-                    if args.each_function:
-                        funcs = [n for n, attr in cfg.nodes(data=True) if attr["label"] == "FUNC_ENTRY"]
-                        for func_entry in funcs:
-                            func_cfg = nx.subgraph(cfg, nx.descendants(cfg, func_entry) | {func_entry})
-                            draw_cfg(func_cfg, entry=func_entry)
-                    else:
-                        draw_cfg(cfg)
+            cfg = CFGCreator.make_cfg(ast)
+            if args.draw_cfg:
+                if args.each_function:
+                    funcs = [n for n, attr in cfg.nodes(data=True) if attr["label"] == "FUNC_ENTRY"]
+                    for func_entry in funcs:
+                        func_cfg = nx.subgraph(cfg, nx.descendants(cfg, func_entry) | {func_entry})
+                        draw_cfg(func_cfg, entry=func_entry)
+                else:
+                    draw_cfg(cfg)
             if args.write_cfg:
-                    for n in cfg.nodes():
-                        del cfg.nodes[n]["n"]
-                    with open(str(filename) + ".graph.json", "w") as of:
-                        json.dump(json_graph.node_link_data(cfg, attrs=None), of, indent=2)
+                for n in cfg.nodes():
+                    del cfg.nodes[n]["n"]
+                with open(str(filename) + ".graph.json", "w") as of:
+                    json.dump(json_graph.node_link_data(cfg, attrs=None), of, indent=2)
             print("successfully parsed", filename)
+
+            if args.draw_duc:
+                duc = make_duc(cfg)
+                pos = nx.drawing.nx_agraph.graphviz_layout(duc, prog='dot')
+                nx.draw(duc, pos=pos, labels={n: attr["label"] for n, attr in duc.nodes(data=True)}, with_labels = True)
+                nx.draw_networkx_edge_labels(duc, pos=pos, edge_labels={(u, v): attr.get("label", "") for u, v, attr in duc.edges(data=True)})
+                plt.show()
         except Exception:
             print("could not parse", filename)
             traceback.print_exc()
