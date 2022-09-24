56c711fecab82a4038487eefe250fd3eed925888
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-09-23 20:07:36 -0500

    Auto-clone tree-sitter libs to home directory

--- treehouse/config.py
+++ treehouse/config.py
@@ -4,6 +4,6 @@ Global configuration
 
 import os
 
-
-TREE_SITTER_C_PREFIX = os.environ.get("treehouse__TREE_SITTER_C_PREFIX", "lib")
+TREEHOUSE_DATAROOT = os.path.expanduser("~/.treehouse/")
+TREE_SITTER_LIB_PREFIX = os.environ.get("treehouse__TREE_SITTER_LIB_PREFIX", os.path.join(TREEHOUSE_DATAROOT, "lib"))
 DRAW_CFG = os.environ.get("treehouse__DRAW_CFG", False)
