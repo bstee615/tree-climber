ac89b43d259404f36fb38b8444814b9efb09adce
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-10-03 13:59:43 -0500

    Introduce new method get_ast

---
+++ tests/test_tree_sitter_utils.py
@@ -0,0 +1,12 @@
+from tree_climber.tree_sitter_utils import c_parser, get_ast
+
+def test_get_ast():
+    code = """int main() {
+    int x = 0;
+    x += 5;
+    return x;
+}
+"""
+    tree = c_parser.parse(bytes(code, "utf8"))
+    ast = get_ast(tree.root_node)
+    print(ast)
