014f65f20158d5c5435e15b79129d2d1d2d83293
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-10-04 06:41:28 -0500

    Comment out unused label escape

--- tree_climber/drawing_utils.py
+++ tree_climber/drawing_utils.py
@@ -1,8 +1,22 @@
+import copy
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
+def escape_labels(graph):
+    graph = copy.deepcopy(graph)
+    for n, d in graph.nodes(data=True):
+        for k in list(d.keys()):
+            if k == "label":
+                if ":" in graph.nodes[n][k]:
+                    graph.nodes[n][k] = f'"{graph.nodes[n][k]}"'
+            else:
+                del graph.nodes[n][k]
+    return graph
+
+
 def draw_ast(ast):
+    # ast = escape_labels(ast)
     pos = nx.drawing.nx_agraph.graphviz_layout(ast, prog="dot")
     nx.draw(
         ast,
@@ -14,6 +28,7 @@ def draw_ast(ast):
 
 
 def draw_cfg(cfg, entry=None):
+    # cfg = escape_labels(cfg)
     if cfg.number_of_nodes() > 1000 or cfg.number_of_edges() > 1000:
         print("fuck man I'm not drawing that!")
         return
@@ -30,6 +45,7 @@ def draw_cfg(cfg, entry=None):
 
 
 def draw_duc(duc):
+    # duc = escape_labels(duc)
     pos = nx.drawing.nx_agraph.graphviz_layout(duc, prog="dot")
     nx.draw(
         duc,
