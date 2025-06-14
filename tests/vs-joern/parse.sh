a9c4369a22a26eb0edd9683fde765ddce7acc8f2
bstee615 <benjaminjsteenhoek@gmail.com>
2025-06-13 23:59:06 -0500

    Start fresh

--- tests/vs-joern/parse.sh
+++
@@ -1,8 +0,0 @@
-if [ $1 == "--joern" ]
-then
-rm -r workspace
-time ./joern/joern-cli/joern --script $(dirname $0)/get_func_graph.scala --params filename=$2
-elif [ $1 == "--tree-sitter" ]
-then
-time python main.py $2 --cfg --file
-fi
--- tree_climber/base_parser.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-
-class BaseParser(abc.ABC):
-    @staticmethod
-    @abc.abstractmethod
-    def parse(data, *args, **kwargs):
-        pass
